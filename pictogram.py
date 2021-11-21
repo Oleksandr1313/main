608dc26c79740ab54087451267d065e03ca7061a
Oleksandr1313 <64073857+Oleksandr1313@users.noreply.github.com>
2021-11-21 18:13:48 +0200

    Update pictogram.py

--- pictogram.py
+++ pictogram.py
@@ -1,6 +1,8 @@
 from turtle import *
+import random
 
 colors = ['red', 'white', 'yellow', 'blue', 'green', 'purple', '#ff69b4', 'cyan', 'magenta', '#faebd7', '#2e8b57', '#eeefff', '#da70d6', '#ff7f50', '#cd853f', '#bc8f8f', '#5f9ea0', '#daa520']
+random.shuffle(colors)
 
 def pictogram(colors):
     bgcolor('black')
